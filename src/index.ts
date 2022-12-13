05fbb946dc3b36b216fbb768e97acbd50718e0c4
Renat <jeingo20@gmail.com>
2022-12-13 15:17:12 +0300

    base express code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import {productRouter} from './routers/product-router'
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser())
+
+app.use('/products', productRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is starting on port: ${PORT}`)
+})
---
+++ src/routers/product-router.ts
@@ -0,0 +1,7 @@
+import {Request, Response, Router} from 'express'
+
+export const productRouter = Router({})
+
+productRouter.get('/', (req:Request, res: Response) => {
+    res.send('OK')
+})
